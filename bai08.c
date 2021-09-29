9fc9b7ad5e9464bf0cd31f178cf22b47d2beb2f5
Chutiendat26092003 <91305197+Chutiendat26092003@users.noreply.github.com>
2021-09-29 14:04:19 +0700

    Add files via upload

---
+++ bai08.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("*******kich thuoc cua tung kieu du lieu*******\n");
+	
+	  printf("     kieu int: 4 byte\n");
+	  printf("     so nguyen: 15\n\n");
+	  
+	  printf("     kieu float: 4 byte\n");
+	  printf("     so thuc kieu float:3.456000\n\n");
+	  
+	  printf("     kieu double: 8 byte\n");
+	  printf("     so thuc kieu double: 3.4567891235\n\n");
+	  
+	  printf("     kieu char: 1 byte\n");
+	  printf("     ky tu: E\n\n");
+	  
+	  printf("     kieu long int: 4 byte\n\n");
+	  
+	  printf("     kieu long double: 12 byte\n");
+	   
+	  
+	    
+	return 0;
+}
